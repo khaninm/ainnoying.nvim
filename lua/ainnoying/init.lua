3aae3193409d6dbad2ef237c76809d9c61ea2e0e
Mikhail Khanin <mkhaninm@gmail.com>
2026-05-04 21:28:56 +0300

    actually fix typo

--- lua/ainnoying/init.lua
+++ lua/ainnoying/init.lua
@@ -47,7 +47,7 @@ M.new_silent_chat = function(completion_item)
 
 	local tools = ""
 	for _, tool in ipairs(completion_item.strategy.codecompanion_tools) do
-		tools = tools .. M.codecompanion_tool_template:format(tool)
+		tools = tools .. M.config.codecompanion_tool_template:format(tool)
 	end
 
 	local current_buffer = ""
