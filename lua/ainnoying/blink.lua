36878ca0061befdd55cf7a85f5d2208354e11a14
Mikhail Khanin <mkhaninm@gmail.com>
2026-05-04 21:06:51 +0300

    init

---
+++ lua/ainnoying/blink.lua
@@ -0,0 +1,91 @@
+local ainnoying = require("ainnoying")
+
+-- the highlighting part
+
+local ns = vim.api.nvim_create_namespace("ainnoying")
+vim.api.nvim_set_hl(0, "ainnoying", { link = "search" })
+
+local function highlight_line(row, offset, cutoff)
+	vim.highlight.range(0, ns, "ainnoying", { row, offset }, { row, cutoff }, { regtype = 'v' })
+end
+
+-- the blink.cmp source part
+
+local source = {}
+
+function source.new(opts)
+	-- honestly, no clue what it does, just copy pasted it from `blink.cmp` boilerplate
+	local self = setmetatable({}, { __index = source })
+	self.opts = opts
+	return self
+end
+
+function source:enabled()
+	local line = vim.api.nvim_get_current_line()
+	for _, strategy in ipairs(ainnoying.config.strategies) do
+		if strategy.predicate(line) then
+			return true
+		end
+	end
+
+	return false
+end
+
+function source:get_completions(ctx, callback)
+	local commentstring = vim.bo.commentstring
+	if commentstring == '' or commentstring == nil then
+		commentstring = '# %s'
+	end
+
+	local items = {}
+
+	for _, strategy in ipairs(ainnoying.config.strategies) do
+		if strategy.predicate(ctx.line) then
+			local whitespace, query = strategy.parse(ctx.line)
+			local completion = whitespace .. commentstring:format(strategy.blink_label_prefix .. query)
+
+			table.insert(items, {
+				label = completion,
+				textEdit = {
+					newText = completion,
+					range = {
+						['start'] = { line = ctx.bounds.line_number - 1, character = 0 },
+						['end'] = { line = ctx.bounds.line_number - 1, character = ctx.bounds.length + completion:len() - 1 },
+					},
+				},
+				documentation = {
+					kind = 'markdown',
+					value = strategy.blink_label_doc
+				},
+				whitespace_len = #whitespace,
+				completion_len = #completion,
+				query = query,
+				strategy = strategy,
+			})
+		end
+	end
+
+	callback({
+		items = items,
+		is_incomplete_backward = ainnoying.config.suggest_on_delete,
+		is_incomplete_forward = true,
+	})
+
+	return function() end
+end
+
+function source:execute(ctx, item, callback, default_implementation)
+	-- invoke AI
+	ainnoying.new_silent_chat(item)
+
+	-- default accept
+	default_implementation()
+	callback()
+
+	-- highlighting
+	if ainnoying.config.highlight then
+		highlight_line(ctx.bounds.line_number - 1, item.whitespace_len, item.completion_len)
+	end
+end
+
+return source
---
+++ lua/ainnoying/init.lua
@@ -0,0 +1,90 @@
+local M = {}
+
+M.config = {
+	suggest_on_delete = true,
+	highlight = true,
+	codecompanion_tool_template = "Use @{%s}. ",
+	strategies = {
+		{
+			blink_label_prefix = "Ask AI: ",
+			blink_label_doc = "Ask AI and come back to the answer whenever you want",
+			parse = function(line) return line:match("^(%s*)%?%s*(.+)") end,
+			predicate = function(line) return line:match("^(%s*)%?%s*(.+)") ~= nil end,
+			codecompanion_message_template = "Answer the following question: %s",
+			codecompanion_tools = {},
+			codecompanion_context = {},
+			codecompanion_include_buffer = false,
+			codecompanion_auto_submit = true,
+		},
+		{
+			blink_label_prefix = "Ask AI: ",
+			blink_label_doc = "Ask AI about your code",
+			parse = function(line) return line:match("^(%s*)%?%s*(.+)") end,
+			predicate = function(line) return line:match("^(%s*)%?%s*(.+)") ~= nil end,
+			codecompanion_message_template = "Answer the following question: %s",
+			codecompanion_tools = {},
+			codecompanion_context = {},
+			codecompanion_include_buffer = true,
+			codecompanion_auto_submit = true,
+		},
+		{
+			blink_label_prefix = "Write code:",
+			blink_label_doc = "AI codes for you",
+			parse = function(line) return line:match("^(%s*)%!%s*(.+)") end,
+			predicate = function(line) return line:match("^(%s*)%!%s*(.+)") ~= nil end,
+			codecompanion_message_template = "%s",
+			codecompanion_tools = { "insert_edit_into_file" },
+			codecompanion_context = {},
+			codecompanion_include_buffer = true,
+			codecompanion_auto_submit = true,
+		},
+	}
+}
+M.chats = {}
+
+M.new_silent_chat = function(completion_item)
+	local codecompanion = require("codecompanion")
+
+	local tools = ""
+	for _, tool in ipairs(completion_item.strategy.codecompanion_tools) do
+		tools = tools .. completion_item.strategy.codecompanion_tool_template:format(tool)
+	end
+
+	local current_buffer = ""
+	if completion_item.strategy.codecompanion_include_buffer then
+		current_buffer = "#buffer "
+	end
+
+	local codecompanion_prefix = current_buffer .. tools
+	local codecompanion_query = completion_item.strategy.codecompanion_message_template:format(completion_item.query)
+
+	local hidden_chat = codecompanion.chat({
+		hidden = true,
+		auto_submit = completion_item.strategy.codecompanion_auto_submit,
+		context = completion_item.strategy.codecompanion_context,
+		messages = { { role = "user", content = codecompanion_prefix .. codecompanion_query }, }
+	})
+
+	if hidden_chat == nil or hidden_chat.id == nil then
+		return
+	end
+
+	table.insert(M.chats,
+		{ chat = hidden_chat, completion = completion_item.label, query = completion_item.strategy.query })
+end
+
+M.open_hidden_chat = function()
+	local current_line = vim.api.nvim_get_current_line()
+
+	for _, chat in ipairs(M.chats) do
+		if current_line == chat.completion then
+			chat.chat.ui:open()
+		end
+	end
+end
+
+M.setup = function(opts)
+	vim.tbl_deep_extend("force", M.config, opts or {})
+end
+
+return M
